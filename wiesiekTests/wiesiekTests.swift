12e8f0cddc689081bbc8485b1367399609d8aefd
Eustachy <eustachy@Eustachys-Mac.local>
2017-09-16 12:48:03 +0200

    Initial Commit

---
+++ wiesiekTests/wiesiekTests.swift
@@ -0,0 +1,36 @@
+//
+//  wiesiekTests.swift
+//  wiesiekTests
+//
+//  Created by Eustachy on 16.09.2017.
+//  Copyright © 2017 Eustachy. All rights reserved.
+//
+
+import XCTest
+@testable import wiesiek
+
+class wiesiekTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
